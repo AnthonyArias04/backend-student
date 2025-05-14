61d6742532aef34873a85d0790a4f588c6ee5b52
AnthonyArias04 <anthonyarias842@gmail.com>
2025-05-13 23:10:15 -0600

    PRIMERA PRUEBA

---
+++ backend-estudiantes/models/Estudiante.js
@@ -0,0 +1,10 @@
+// models/Estudiante.js
+const mongoose = require('mongoose');
+ 
+const estudianteSchema = new mongoose.Schema({
+  nombre: String,
+  carrera: String,
+  edad: Number
+});
+ 
+module.exports = mongoose.model('Estudiante', estudianteSchema);
